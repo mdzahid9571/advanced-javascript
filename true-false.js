18d6b5610d5e9d40a1a810ba4d94300cf07f1eab
zahid <mdzahid9571@gmail.com>
2021-02-03 02:09:44 +0600

    already add

---
+++ true-false.js
@@ -0,0 +1,20 @@
+//False:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truth
+// '0'
+// ' '
+// []
+
+let name = 'false';
+if(name){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
